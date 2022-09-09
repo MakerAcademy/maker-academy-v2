0642025b14cea37d547194a03fe7f83c6c39093d
Salman Fazal <salmanfazal01@gmail.com>
2022-09-09 10:10:15 -0400

    Profile

--- src/components/ProfileBanner/index.js
+++ src/components/ProfileBanner/index.js
@@ -3,12 +3,19 @@ import EmailIcon from "@mui/icons-material/Email";
 import FileCopyIcon from "@mui/icons-material/FileCopy";
 import LanguageIcon from "@mui/icons-material/Language";
 import TelegramIcon from "@mui/icons-material/Telegram";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import WorkIcon from "@mui/icons-material/Work";
 import {
   Avatar,
   Box,
   Button,
   Grid,
+  Link,
   Paper,
   Stack,
   Tab,
@@ -35,6 +42,35 @@ const CustomPaper = ({ title, children }) => (
   </Paper>
 );
 
+const SocialLink = ({ href = "#", Icon, text, disabled, color, ...other }) => (
+  <Button
+    href={href}
+    target="_blank"
+    sx={{ width: "fit-content", minWidth: 115 }}
+    disabled={!!disabled}
+    {...other}
+  >
+    <Stack
+      direction="row"
+      alignItems="center"
+      justifyContent="flex-start"
+      spacing={1}
+      sx={{ width: "100%" }}
+    >
+      <Icon
+        fontSize="small"
+        sx={{ color: disabled ? "grey.grey3" : color || "text.primary" }}
+      />
+      <Typography
+        variant="body2"
+        sx={{ color: disabled ? "grey.grey3" : "text.primary" }}
+      >
+        {text}
+      </Typography>
+    </Stack>
+  </Button>
+);
+
 const ProfileBanner = ({
   profilePicture,
   name,
@@ -52,65 +88,6 @@ const ProfileBanner = ({
   const handleTabChange = (event, newValue) => {
     setTabValue(newValue);
   };
-
-  const AddressBox = ({ address }) => (
-    <Tooltip title="Copy to clipboard">
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-        spacing={1}
-        sx={{
-          borderRadius: "20px",
-          py: 1,
-          px: 2,
-          backgroundColor: "primary.invert",
-          cursor: "pointer",
-          "&:hover": {
-            color: theme.palette.primary.purple,
-          },
-          [theme.breakpoints.up("sm")]: {
-            maxWidth: 300,
-          },
-        }}
-      >
-        <Box
-          sx={{
-            width: "85%",
-          }}
-        >
-          <Typography
-            variant="body2"
-            sx={{
-              textOverflow: "ellipsis",
-              whiteSpace: "nowrap",
-              overflow: "hidden",
-            }}
-          >
-            {address}
-          </Typography>
-        </Box>
-
-        <FileCopyIcon fontSize="small" />
-      </Stack>
-    </Tooltip>
-  );
-
-  const SocialLink = ({ children }) => (
-    <Button
-      variant="outlined"
-      sx={{
-        width: 40,
-        height: 40,
-        minWidth: 0,
-        borderRadius: "8px",
-        bgcolor: "primary.invert",
-        border: `1px solid primary.invert`,
-      }}
-    >
-      {children}
-    </Button>
-  );
 
   return (
     <Box sx={{ ...sx }}>
@@ -133,6 +110,7 @@ const ProfileBanner = ({
           direction={{ xs: "column", md: "row" }}
           alignItems={{ md: "flex-end" }}
           spacing={{ xs: 3, md: 5 }}
+          sx={{ height: "100%" }}
         >
           <Avatar
             src={profilePicture}
@@ -164,15 +142,57 @@ const ProfileBanner = ({
       <Grid container sx={{ pt: 4 }} spacing={5}>
         <Grid item xs={12} md={6}>
           <CustomPaper title="About">
-            <Stack spacing={1}>
+            <Stack spacing={1.5}>
               <Typography variant="body2">{bio}</Typography>
+
+              <SocialLink
+                href={`mailto:${socials?.publicEmail}`}
+                Icon={EmailIcon}
+                text={socials?.publicEmail}
+                disabled={!socials?.publicEmail}
+              />
+
+              <SocialLink
+                // href={`mailto:${socials?.publicEmail}`}
+                Icon={WorkIcon}
+                text={title}
+              />
             </Stack>
           </CustomPaper>
         </Grid>
 
         <Grid item xs={12} md={6}>
           <CustomPaper title="Socials">
-            <Stack spacing={1}></Stack>
+            <Stack spacing={1.5}>
+              <SocialLink
+                href={`https://www.facebook.com/${socials?.facebook}`}
+                Icon={FacebookIcon}
+                text={socials?.facebook}
+                disabled={!socials?.facebook}
+                color="#4267B2"
+              />
+              <SocialLink
+                href={`https://www.instagram.com/${socials?.instagram}`}
+                Icon={InstagramIcon}
+                text={socials?.instagram}
+                disabled={!socials?.instagram}
+                color="#CA295F"
+              />
+              <SocialLink
+                href={`https://www.linkedin.com/${socials?.linkedin}`}
+                Icon={LinkedInIcon}
+                text={socials?.linkedin}
+                disabled={!socials?.linkedin}
+                color="#0e76a8"
+              />
+              <SocialLink
+                href={`https://www.linkedin.com/${socials?.twitter}`}
+                Icon={TwitterIcon}
+                text={socials?.twitter}
+                disabled={!socials?.twitter}
+                color="#00ACEE"
+              />
+            </Stack>
           </CustomPaper>
         </Grid>
       </Grid>
