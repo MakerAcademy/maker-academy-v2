dd3714acda7b298b19eb934957a81e24d7f30de3
Salman Fazal <salmanfazal01@gmail.com>
2023-02-06 18:38:58 -0500

    Storybook init

---
+++ src/stories/GreenButton.stories.js
@@ -0,0 +1,28 @@
+import GreenButton from "../components/buttons/GreenButton";
+
+export default {
+  title: "Main Button",
+  component: GreenButton,
+};
+
+const Template = (args) => <GreenButton {...args}>Button</GreenButton>;
+
+export const Contained = Template.bind({});
+Contained.args = {
+  variant: "contained",
+};
+
+export const Outlined = Template.bind({});
+Outlined.args = {
+  variant: "outlined",
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: "small",
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: "large",
+};
